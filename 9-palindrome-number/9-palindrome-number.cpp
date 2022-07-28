ada12376db5be2817b4dab26148fbc0df645dfd1
Smriti Kaushal <73057118+smriti2410@users.noreply.github.com>
2022-07-29 01:39:18 +0530

    Time: 17 ms (69.19%), Space: 5.8 MB (90.84%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-          string rev = to_string(x);
-        reverse(rev.begin(), rev.end());
-        return to_string(x) == rev;
+        long rev=0;
+        long temp=x;
+        
+        while(x>0){
+            long digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+            
+        }
+        
+        if(rev==temp)
+            return true;
+        
+        else 
+            return false;
     }
 };
