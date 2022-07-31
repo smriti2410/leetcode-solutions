e07c1e2db25a011eed05ecb377946f0b29461ec2
Smriti Kaushal <73057118+smriti2410@users.noreply.github.com>
2022-07-31 17:56:41 +0530

    Time: 14 ms (48.11%), Space: 13.7 MB (17.69%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-         vector<int> ans(2, -1);
-        int start = 0, end = (int)nums.size() - 1;
-        while(start <= end) {
-            int mid = start + (end-start)/2;
-            if(nums[mid] < target) start = mid+1;
+         vector<int> ans(2, -1);// creating answer vector{-1,-1}
+        int start = 0, end = (int)nums.size() - 1; //start and end index for binary search at array is sorted 
+        while(start <= end) { //1st occurance 
+            int mid = start + (end-start)/2;//calc mid
+            if(nums[mid] < target) start = mid+1; // mid val is less than target therefor target lies on right hence start index will start after mid and search right half array
             else {
-                if(nums[mid] == target) ans[0] = mid;
-                end = mid-1;
+                if(nums[mid] == target) ans[0] = mid;// if target == mid // 1st occurance found
+                end = mid-1; // else search left half array by shifting end to mid-1
             }
         }
         start = 0, end = (int)nums.size() - 1;
         while(start <= end) {
-            int mid = start + (end-start)/2;
+            int mid = start + (end-start)/2;// 2nd occurance 
             if(nums[mid] > target) end = mid-1;
             else {
                 if(nums[mid] == target) ans[1] = mid;
