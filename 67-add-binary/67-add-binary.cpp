0b96ae8c1acb444fc59e46b3dff43472aa2b6a69
Smriti Kaushal <73057118+smriti2410@users.noreply.github.com>
2022-08-01 12:29:40 +0530

    Time: 3 ms (76.14%), Space: 6.3 MB (84.58%) - LeetHub

---
+++ 67-add-binary/67-add-binary.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        
+        string ans;
+        
+        int m=a.length()-1;
+        int n=b.length()-1;
+        
+        int carry=0;
+        
+     while(m>=0||n>=0){
+         int sum=carry;
+         
+        
+          if(m>=0)
+             sum+=(a[m--])-'0';
+         
+          if(n>=0)
+             sum+=(b[n--])-'0';
+        
+         if(sum>1)
+             carry=1;
+         else
+             carry=0; 
+         ans+=to_string(sum%2);
+         
+     }
+        if(carry)
+            ans+=to_string(carry);
+        
+        
+       reverse(ans.begin(),ans.end()); 
+        
+        return ans;
+    }
+    
+};
