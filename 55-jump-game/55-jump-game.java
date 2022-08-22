15502ce9bd5e64078269957520742b2023af5c82
Smriti Kaushal <73057118+smriti2410@users.noreply.github.com>
2022-08-22 14:29:56 +0530

    Time: 194 ms (22.91%), Space: 69.4 MB (9.06%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,12 +1,25 @@
-class Solution {
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
+public class Solution {
     public boolean canJump(int[] nums) {
-        int max =0;
-          for(int i=0; i<nums.length; i++){
-              if(i>max)return false;
-              max= Math.max(nums[i]+i , max);
-            
-              
-          }
-        return true;
+        Index[] memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+
+        return memo[0] == Index.GOOD;
     }
 }
