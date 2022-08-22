a17b7ad584df6c9e8db94bed376a4e83194b32fc
Smriti Kaushal <73057118+smriti2410@users.noreply.github.com>
2022-08-22 12:30:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (57.31%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+          int t[46]={0};
+    int climbStairs(int n) {
+  
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        
+        else if(t[n]!=0)
+            return t[n];
+        
+        t[n]=climbStairs(n-1)+climbStairs(n-2);
+        
+        return t[n];
+    
+        
+        
+        
+        
+        
+    }
+};
